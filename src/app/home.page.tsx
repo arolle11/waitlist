b82e3944345e49d28efc86fbd724da1c2fdf46f6
Arolle11 <arollefona11@gmail.com>
2025-06-26 23:13:50 +0100

    first commit

--- src/app/home.page.tsx
+++ src/app/home.page.tsx
@@ -1,22 +1,101 @@
 import Button from "@/components/common/button";
 import Input from "@/components/common/input";
-import { PageComponent } from "rasengan";
+import Image from "@rasenganjs/image";
+import { Inbox, User, Mail, MoveRight } from "lucide-react";
+import { PageComponent, Link } from "rasengan";
 
 const Home: PageComponent = () => {
-	return (
-		<section className='w-full h-screen flex flex-col items-center justify-center py-8 px-[20px] md:px-[50px] xl:px-[200px] font-raleway'>
-			<p className="text-foreground text-2xl font-bold">Bootcamp Waitlist</p>
+  return (
+    <section className="w-full min-h-screen flex flex-col font-raleway bg-background text-foreground">
+      <div className="fixed -top-[350px] blur-[400px] left-1/2 -translate-x-1/2 size-[300px] bg-violet rounded-full"></div>
+      <header className="w-full h-[80px] px-8 flex justify-end items-center gap-2">
+        <Button className="h-10">Update</Button>
+        <Button className="size-10 p-0 flex justify-center items-center">
+          <Inbox size="16" />
+        </Button>
+      </header>
+      <main className="w-full flex justify-center items-center min-h-[580px]">
+        <div className=" w-full md:w-[500px] flex flex-col justify-start items-center px-4">
+          <Image
+            src="/rasengan.svg"
+            width={80}
+            height={80}
+            alt="Rasengan Logo"
+          />
 
-			<Input placeholder="Enter your email" className="w-[300px] mt-8" />
-			<Button className="mt-4 rounded-md px-8 w-[300px]">Join</Button>
-		</section>
-	);
+          <p className="text-sm font-medium text-foreground/40 mt-4">
+            BOOTCAMP WAITLIST
+          </p>
+          <h1 className="text-4xl font-bold text-center mt-4 text-pretty">
+            <span className="block text-transparent bg-clip-text bg-gradient-to-b from-[#615f67] to-[#a5a6ab]">
+              Join the Waitlist for the
+            </span>
+            <span className="block text-transparent bg-clip-text bg-gradient-to-r from-pink to-violet">
+              Dev girls Bootcamp!
+            </span>
+          </h1>
+          <form className="w-full sm:w-[300px] max-w-[300px] h-auto mt-10">
+            <div className="relative">
+              <Input
+                type="text"
+                placeholder="Full name..."
+                className="w-full text-sm pl-10"
+              />
+              <User
+                size="16"
+                className="absolute top-1/2 -translate-y-1/2 left-3"
+              />
+            </div>
+            <div className="relative mt-4">
+              <Input
+                type="email"
+                placeholder="Emai address..."
+                className="w-full text-sm pl-10"
+              />
+              <Mail
+                size="16"
+                className="absolute top-1/2 -translate-y-1/2 left-3"
+              />
+            </div>
+
+            <Button className="w-full mt-4 text-foreground/60 relative">
+              <MoveRight className="absolute top-1/2 -translate-y-1/2 right-3" />
+              <span className="text-sm">Continue</span>
+            </Button>
+          </form>
+        </div>
+      </main>
+      <footer className="min-h-[150px] text-[12px] w-full flex flex-col items-center justify-center text-foreground/60">
+        <p>Join the waitlist devgirl bootcamp</p>
+        <p className="flex gap-1">
+          <span>Designed by</span>
+          <Link
+            to="https://arollefona.me/"
+            className="text-foreground text-medium"
+          >
+            @arolle
+          </Link>
+          <span>to give you back your time</span>
+        </p>
+
+        <p className="mt-2">
+          Powered by
+          <Link
+            to="https://www.rasengan.dev/"
+            className="text-foreground text-medium"
+          >
+            RasenganJs
+          </Link>
+        </p>
+      </footer>
+    </section>
+  );
 };
-	
+
 Home.path = "/";
 Home.metadata = {
-	title: "Bootcamp Waitlist",
-	description: "Keep track of newest updates from bootcamp",
+  title: "Bootcamp Waitlist",
+  description: "Keep track of newest updates from bootcamp",
 };
 
 export default Home;
--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -3,10 +3,18 @@ import { cn } from "@/lib/utils";
 
 type Props = {
   children: React.ReactNode;
-} & ComponentProps<'button'>  
+} & ComponentProps<"button">;
 
 export default function Button({ children, className, ...props }: Props) {
-	return (
-		<button className={cn('px-4 py-2 bg-primary text-primary-foreground hover:cursor-pointer hover:opacity-80 transition-all', className)} {...props}>{children}</button>
-	)
+  return (
+    <button
+      className={cn(
+        "px-4 py-2 bg-primary text-sm text-primary-foreground hover:cursor-pointer hover:opacity-80 transition-all rounded-md",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
